11b1250a6e30609d910b9c502fb13b7b256af7c9
Archlich <rokasstankunas03@gmail.com>
2025-06-13 14:12:41 +0300

    Removed Vue component

---
+++ docker/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Wait for DB if needed (optional)
+# sleep 10
+
+# Laravel setup
+#chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+#chmod -R 775 /var/www/storage /var/www/bootstrap/cache
+composer install
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan key:generate
+
+# Start PHP-FPM
+exec php-fpm
