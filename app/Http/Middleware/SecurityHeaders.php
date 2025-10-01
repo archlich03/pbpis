0284dad828e0a199e51cd16a4a9e21e737d0033b
rokas <rokas@stankunas.me>
2025-10-01 23:09:26 +0300

    fix:  loading scripts and stylesheets from http://localhost:5173

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -18,7 +18,7 @@ class SecurityHeaders
         $response = $next($request);
 
         // Content Security Policy - restrict resource loading
-        $viteHosts = app()->environment('local') ? " localhost:5173 127.0.0.1:5173 pbpis.teso.fyi:5173" : "";
+        $viteHosts = app()->environment('local') ? " http://localhost:5173 http://127.0.0.1:5173 http://pbpis.teso.fyi:5173 https://pbpis.teso.fyi:5173" : "";
         $viteWs = app()->environment('local') ? " ws://localhost:5173 ws://127.0.0.1:5173 ws://pbpis.teso.fyi:5173 wss://pbpis.teso.fyi:5173" : "";
         
         $csp = "default-src 'self'; ";
