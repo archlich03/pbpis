2ad59341f8bffb48a19fe3dd7e7da7695e463565
rokas <rokas@stankunas.me>
2025-10-04 17:16:01 +0300

    feat: register tiptapEditor as Alpine data component instead of global window object

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,9 @@ import './bootstrap';
 import Alpine from 'alpinejs';
 import tiptapEditor from './tiptap-editor';
 
+// Register tiptapEditor as an Alpine data component
+Alpine.data('tiptapEditor', tiptapEditor);
+
 window.Alpine = Alpine;
-window.tiptapEditor = tiptapEditor;
 
 Alpine.start();
